c262c4b699f5970768877d45bebdac4a6276079a
Florent2020 <60365705+Florent2020@users.noreply.github.com>
2025-06-27 01:22:13 +0200

    added lokal-notodden project

--- src/components/dataProjects.js
+++ src/components/dataProjects.js
@@ -1,3 +1,4 @@
+import lokalnotodden from "../assets/images/lokalnotodden.png";
 import solheimfeltet from "../assets/images/solheimfeltet.png";
 import vigo from "../assets/images/vigo-iks.png";
 import spaceX from "../assets/images/spaceX.png";
@@ -12,60 +13,66 @@ import aquaPharm from "../assets/images/aqua-pharm.png";
 const projects = [
   {
     id: 1,
+    image: lokalnotodden,
+    project: "Lokal-Notodden",
+    url: "https://lokalnotodden.no/",
+  },
+  {
+    id: 2,
     image: solheimfeltet,
     project: "Solheimfeltet",
     url: "https://www.solheimfeltet.no/",
   },
   {
-    id: 2,
+    id: 3,
     image: vigo,
     project: "Vigo IKS",
     url: "https://www.vigoiks.no/",
   },
   {
-    id: 3,
+    id: 4,
     image: spaceX,
     project: "Galaxy SpaceX",
     url: "https://space-x-project.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     image: laiout,
     project: "Laiout",
     url: "https://laiout-one.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     image: nucleus,
     project: "Nucleus",
     url: "https://nucleus-tau.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     image: letha,
     project: "Letha Shoemaker",
     url: "https://letha-shoes-sandy.vercel.app/",
   },
   {
-    id: 7,
+    id: 8,
     image: yay,
     project: "The YAY Company",
     url: "https://yay-company.vercel.app/",
   },
   {
-    id: 8,
+    id: 9,
     image: homeDecor,
     project: "Home Decor",
     url: "https://home-decor-one.vercel.app/",
   },
   {
-    id: 9,
+    id: 10,
     image: winterSports,
     project: "Winter Sports",
     url: "https://winter-sports.vercel.app/",
   },
   {
-    id: 10,
+    id: 11,
     image: aquaPharm,
     project: "Aqua Pharm",
     url: "https://aquapharm-ks.com/",

958c992d81fd40aa114ae563a7622bbe7a7961ba
Florent2020 <60365705+Florent2020@users.noreply.github.com>
2026-03-01 21:30:48 +0100

    updated dataProjects.jsx & PDF

--- src/components/dataProjects.js
+++ src/components/dataProjects.js
@@ -1,3 +1,5 @@
+import AlethraLink from "../assets/images/AlethraLink.png";
+import gtNordics from "../assets/images/gt-nordics.png";
 import lokalnotodden from "../assets/images/lokalnotodden.png";
 import solheimfeltet from "../assets/images/solheimfeltet.png";
 import vigo from "../assets/images/vigo-iks.png";
@@ -13,66 +15,78 @@ import aquaPharm from "../assets/images/aqua-pharm.png";
 const projects = [
   {
     id: 1,
+    image: AlethraLink,
+    project: "AlethraLink",
+    url: "https://alethralink.vercel.app/",
+  },
+  {
+    id: 2,
+    image: gtNordics,
+    project: "G.T Nordics AS",
+    url: "https://gt-nordics-as.vercel.app/",
+  },
+  {
+    id: 3,
     image: lokalnotodden,
     project: "Lokal-Notodden",
     url: "https://lokalnotodden.no/",
   },
   {
-    id: 2,
+    id: 4,
     image: solheimfeltet,
     project: "Solheimfeltet",
     url: "https://www.solheimfeltet.no/",
   },
   {
-    id: 3,
+    id: 5,
     image: vigo,
     project: "Vigo IKS",
     url: "https://www.vigoiks.no/",
   },
   {
-    id: 4,
+    id: 6,
     image: spaceX,
     project: "Galaxy SpaceX",
     url: "https://space-x-project.vercel.app/",
   },
   {
-    id: 5,
+    id: 7,
     image: laiout,
     project: "Laiout",
     url: "https://laiout-one.vercel.app/",
   },
   {
-    id: 6,
+    id: 8,
     image: nucleus,
     project: "Nucleus",
     url: "https://nucleus-tau.vercel.app/",
   },
   {
-    id: 7,
+    id: 9,
     image: letha,
     project: "Letha Shoemaker",
     url: "https://letha-shoes-sandy.vercel.app/",
   },
   {
-    id: 8,
+    id: 10,
     image: yay,
     project: "The YAY Company",
     url: "https://yay-company.vercel.app/",
   },
   {
-    id: 9,
+    id: 11,
     image: homeDecor,
     project: "Home Decor",
     url: "https://home-decor-one.vercel.app/",
   },
   {
-    id: 10,
+    id: 12,
     image: winterSports,
     project: "Winter Sports",
     url: "https://winter-sports.vercel.app/",
   },
   {
-    id: 11,
+    id: 13,
     image: aquaPharm,
     project: "Aqua Pharm",
     url: "https://aquapharm-ks.com/",
